4ebf79cb0c6fb3183fbfc8bae41fc0ec3c406867
Ankit Yadav <mahesh9426346889@gmail.com>
2023-08-26 01:57:54 +0530

    Carousel Slider created

---
+++ gameon/src/app/games/[slug]/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import CarouselSlider from "@/components/CarouselSlider/CarouselSlider";
+
+// import GameDetailsClient from "@/components/GameDetails/GameDetailsClient";
+// import GameDetailsServer from "@/components/GameDetails/GameDetailsServer";
+
+const GameItem = async (props: { params: { slug: string } }) => {
+  const {
+    params: { slug },
+  } = props;
+
+  return (
+    // <GameDetailsClient slug={slug}>
+    //   <GameDetailsServer slug={slug} />
+    // </GameDetailsClient>
+    // <CarouselSlider/>
+    <div></div>
+  );
+};
+
+export default GameItem;
