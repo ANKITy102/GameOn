73af3ea72c6df338c3c2227217b4af9a17fdf7e4
Ankit Yadav <mahesh9426346889@gmail.com>
2023-08-27 17:41:33 +0530

    redux store is setup

--- gameon/src/app/layout.tsx
+++ gameon/src/app/layout.tsx
@@ -1,41 +1,41 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Header from '@/components/Header/Header'
-import { Montserrat, Poppins } from 'next/font/google'
-import Footer from '@/components/Footer/Footer'
+import "./globals.css";
+import type { Metadata } from "next";
+import Header from "@/components/Header/Header";
+import { Montserrat, Poppins } from "next/font/google";
+import Footer from "@/components/Footer/Footer";
+import { Providers } from "@/redux/Providers";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight:["400", "500", "700"],
-  style:["italic", "normal"],
-  variable:"--font-poppins",
-})
+  weight: ["400", "500", "700"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
 const montserrat = Montserrat({
   subsets: ["latin"],
-  weight:["400", "500", "700"],
-  style:["italic", "normal"],
-  variable:"--font-montserrat",
-})
+  weight: ["400", "500", "700"],
+  style: ["italic", "normal"],
+  variable: "--font-montserrat",
+});
 export const metadata: Metadata = {
-  title: 'NextJs Ecommerce',
-  description: 'This is a shop where we sell online games',
-}
+  title: "NextJs Ecommerce",
+  description: "This is a shop where we sell online games",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en"  className={`${poppins.variable} ${montserrat.variable}`}>
-
-      <body >
-        <Header/>
-        <main className="bg-primary-gradient min-h-screen">
-        {children}
-        </main>
-        <Footer/>
-        </body>
+    <html lang="en" className={`${poppins.variable} ${montserrat.variable}`}>
+      <body>
+        <Providers>
+          <Header />
+          <main className="bg-primary-gradient min-h-screen">{children}</main>
+          <Footer />
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
