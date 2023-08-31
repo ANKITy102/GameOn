b2d4bdf961f4aeacae23bb7bdfeef6796f3c630f
Ankit Yadav <mahesh9426346889@gmail.com>
2023-09-01 01:20:29 +0530

    error due to package mistake removed

--- gameon/src/libs/auth.ts
+++ gameon/src/libs/auth.ts
@@ -1,52 +1,54 @@
-import {NextAuthOptions} from "next-auth"
-import {MongoDBAdapter} from "@auth/mongodb-adapter"
-import clientPromise from "./mongodb"
-import GoogleProvider from "next-auth/providers/google"
-import CredentailsProvider from "next-auth/providers/credentials"
-import { MongoClient } from "mongodb"
-export const authOptions:NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        CredentailsProvider({
-            name: 'email and password',
-            credentials: {
-                email: {
-                    label: "Email",
-                    placeholder: "example@gmail.com",
-                    type: "email",
-                },
-                password: {
-                    label: "Password",
-                    type: "password",
-                    placeholder:"Enter Password"
-                }
-            },
-             async authorize (credentaials, req){
+import { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import { MongoClient } from 'mongodb';
 
-                if(!credentaials?.email || !credentaials?.password){
-                    throw new Error("Please provide valid credentails");
-                }
-                const client = await MongoClient.connect(
-                    process.env.MONGO_URI as string
-                )
-                const db = client.db();
-                const user = await db.collection('users').findOne({email:credentaials.email});
-                if(user){
-                    return user as any;
-                }
-                else{
-                    return null;
-                }
-             }
-        })
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    adapter: MongoDBAdapter(clientPromise),
-    debug: process.env.NODE_ENV==="development",
-    secret: process.env.NEXTAUTH_SECRET,
-}
+import clientPromise from './mongodb';
+
+export const authOptions: NextAuthOptions = {
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+		CredentialsProvider({
+			name: 'email and password',
+			credentials: {
+				email: {
+					label: 'Email',
+					placeholder: 'email@email.com',
+					type: 'email',
+				},
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials, req) {
+				if (!credentials?.email || !credentials?.password) {
+					throw new Error('Please provide valid credentials');
+				}
+
+				const client = await MongoClient.connect(
+					process.env.MONGODB_URI as string
+				);
+
+				const db = client.db();
+
+				const user = await await db
+					.collection('users')
+					.findOne({ email: credentials.email });
+
+				if (user) {
+					return user as any;
+				} else {
+					return null;
+				}
+			},
+		}),
+	],
+	session: {
+		strategy: 'jwt',
+	},
+	adapter: MongoDBAdapter(clientPromise),
+	debug: process.env.NODE_ENV === 'development',
+	secret: process.env.NEXTAUTH_SECRET,
+};
